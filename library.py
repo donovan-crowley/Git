d78834c56e337e94bad66456ebe5e259c0a663df
Donovan Crowley <donocrowley16@gmail.com>
2025-05-31 16:54:52 -0400

    Add an argument parser

--- library.py
+++ library.py
@@ -3,3 +3,6 @@ from datetime import datetime
 from fnmatch import fnmatch
 from math import ceil
 
+argparser = argparse.ArgumentParser(description = "Command-line parser")
+argsubparser = argparser.add_argument(title = "Command", dest = "command")
+argsubparser.required = True
