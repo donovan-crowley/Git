71777befeae38f616e3c2b59053a526e4fccecb2
Donovan Crowley <donocrowley16@gmail.com>
2025-06-02 20:41:07 -0400

    Create object class and read objects

--- libGit.py
+++ libGit.py
@@ -156,3 +156,50 @@ def repo_find(path = ".", required = True):
 
 def cmd_init(args):
     repo_create(args.path)
+
+class GitObject(object):
+    def __init__(self, data = None):
+        if(data != None):
+            self.deserialize(data)
+        else:
+            self.init()
+    
+    def serialize(self, repo):
+        raise Exception("Unimplemented!")
+    
+    def deserialize(self, data):
+        raise Exception("Unimplemented!")
+    
+    def init(self):
+        pass
+
+def object_read(repo, sha):
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+    if not os.path.isfile(open):
+        return None
+    
+    with open (path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        # Raw object type
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        # Read and validate object size
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw) - y - 1:
+            raise Exception(f"Malformed object {sha}: bad length")
+        
+        # Pick constructor
+        match fmt:
+            case b'commit' : c = GitCommit
+            case b'tree' : c = GitTree
+            case b'tag' : c = GitTag
+            case b'blob' : c = GitBlob
+            case _: raise Exception(f"Unknown type {fmt.decode("ascii")} for object {sha}")
+        # Call constructor and return object
+        return c(raw[y + 1:])
+    
+            
